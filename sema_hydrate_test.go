115a69aa4684687829aeb86cd67b07f172e4e410
Herman Banken <herman@q42.nl>
2020-08-19 00:13:47 +0200

    Extra hydrate tests

--- sema_hydrate_test.go
+++ sema_hydrate_test.go
@@ -8,28 +8,65 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHydratedPopulate(t *testing.T) {
-
-	// nil
+func TestHydrateNil(t *testing.T) {
 	var tree *convictJSONTree
 	result, err := hydrateSecretTree(tree, map[string]resolvedSecret{})
 	assert.Equal(t, nil, err)
 	assert.Equal(t, nil, result)
+}
 
-	// simple tree
+func TestHydrateFlatTree(t *testing.T) {
+	client := secretmanager.NewMockClient("my-project", "log_level", "warn")
+
 	schema, err := parseSchema([]byte(`{
     /* logging config */
     "LOG_FORMAT": { "format": ["json", "text"], "default": "json", "doc": "How to log" },
     "LOG_LEVEL": { "format": ["warn", "error"], "default": "error", "doc": "When to log" },
 }`))
-	client = secretmanager.NewMockClient("my-project", "log_level", "info")
-	result, err = hydrateSecretTree(schema.tree, map[string]resolvedSecret{
+	result, err := hydrateSecretTree(schema.tree, map[string]resolvedSecret{
 		"LOG_FORMAT": resolvedSecretRuntime{*schema.tree.Children["LOG_FORMAT"].Leaf},
-		"LOG_LEVEL":  resolvedSecretSema{key: "log_level"},
+		"LOG_LEVEL":  resolvedSecretSema{key: "log_level", client: client},
 	})
 	jsonData, _ := json.MarshalIndent(result, "", "  ")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, `{
-  "LOG_LEVEL": "info"
+  "LOG_LEVEL": "warn"
+}`, string(jsonData))
+
+}
+
+func TestHydrateNestedTree(t *testing.T) {
+	client := secretmanager.NewMockClient("my-project", "logging_level", "warn")
+
+	schema, err := parseSchema([]byte(`{
+    /* logging config */
+    "LOGGING": {
+      "FORMAT": { "format": ["json", "text"], "default": "json", "doc": "How to log" },
+      "LEVEL": { "format": ["warn", "error"], "default": "error", "doc": "When to log" },
+    }
+}`))
+	assert.Equal(t, nil, err)
+	assert.NotNil(t, schema.tree.Children["LOGGING"], "LOGGING")
+	assert.NotNil(t, schema.tree.Children["LOGGING"].Children["FORMAT"], "FORMAT")
+	assert.NotNil(t, schema.tree.Children["LOGGING"].Children["LEVEL"], "LEVEL")
+
+	// One is runtime, other is resolved
+	resolved := schemaResolver{Client: client, Verbose: true}.Resolve(schema)
+	assert.IsType(t, resolvedSecretRuntime{}, resolved["LOGGING.FORMAT"], "LOGGING.FORMAT")
+	assert.IsType(t, resolvedSecretSema{}, resolved["LOGGING.LEVEL"], "LOGGING.LEVEL")
+	assert.Equal(t, client, resolved["LOGGING.LEVEL"].(resolvedSecretSema).client, "LOGGING.LEVEL")
+
+	result, err := hydrateSecretTree(schema.tree.Children["LOGGING"].Children["FORMAT"], resolved)
+	assert.Equal(t, nil, result)
+
+	result, err = hydrateSecretTree(schema.tree, resolved)
+	levelValue, err := resolved["LOGGING.LEVEL"].GetSecretValue()
+	assert.Equal(t, map[string]interface{}(map[string]interface{}{"LOGGING": map[string]interface{}{"LEVEL": levelValue}}), result)
+	jsonData, _ := json.MarshalIndent(result, "", "  ")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, `{
+  "LOGGING": {
+    "LEVEL": "warn"
+  }
 }`, string(jsonData))
 }
